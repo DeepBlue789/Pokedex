caefa930356c32f7eaa53a130f02ff9651b436ad
Nolwenn 22 <lotier77@gmail.com>
2020-11-07 20:43:39 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const searchInput = document.querySelector('.recherche-poke input');
+
+
+function fetchPokemonBase(){
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then(reponse => reponse.json())
+        .then((allPoke) => {
+            console.log(allPoke);
+        })
+}
+
+fetchPokemonBase();
+
+//Animation Input
+
+searchInput.addEventListener('input', e => {
+
+    if(e.target.value !== ''){
+        e.target.parentNode.classList.add('active-input');
+    }else{
+        e.target.parentNode.classList.remove('active-input');
+    }
+})
